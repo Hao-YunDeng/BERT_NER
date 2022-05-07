040a677be243293dbb685b20c089537343d226a0
Haoyun Deng <41811221+Hao-YunDeng@users.noreply.github.com>
2022-05-07 03:44:14 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -27,9 +27,9 @@ MAX_LEN = 128
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
-BASE_MODEL_PATH = "../input/bert-base-uncased"
+BASE_MODEL_PATH = "../base_model/bert-base-uncased"
 MODEL_PATH = "model.bin" # This is where to save the model
-TRAINING_FILE = "../input/ner_dataset.csv"
+TRAINING_FILE = "../data/ner_dataset.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BASE_MODEL_PATH,
     do_lower_case=True
